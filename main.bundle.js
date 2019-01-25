a7c015952782e2f1b332775420e1f5b2cb5f760a
amypetrie <apetrie9849@gmail.com>
2019-01-24 18:47:02 -0700

    Replace script src tag to point to correct bundle file.

--- main.bundle.js
+++ main.bundle.js
@@ -44,8 +44,132 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-	// This file is in the entry point in your webpack config.
 	"use strict";
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	// var env = require("styles.css");
+	var getWeatherButton = document.getElementById("submitLocation");
+	var weatherInput = document.getElementById("locationSearchInput");
+	var apiUrl = "https://weather-bug.herokuapp.com/";
+	// var apiUrl = "http://localhost:3000/";
+	var apiResponse = null;
+	var weatherForecastObj = null;
+	var userApiKey = null;
+	var userFavoritesObj = null;
+
+	window.onload = setUserVariabes();
+
+	var WeatherForecast = function WeatherForecast(weatherData) {
+	  _classCallCheck(this, WeatherForecast);
+
+	  // box one
+	  this.locationName = weatherData["attributes"]["location_description"]["formattedAddress"];
+	  this.shortDescription = weatherData["attributes"]["daily_forecast"]["current_summary"];
+	  this.currentTemp = weatherData["attributes"]["daily_forecast"]["current_temp"];
+	  this.currentHigh = weatherData["attributes"]["daily_forecast"]["temperature_high"];
+	  this.currentLow = weatherData["attributes"]["daily_forecast"]["temperature_low"];
+	  this.currentTime = weatherData["attributes"]["daily_forecast"]["time"];
+	  // box two
+	  this.feelsLike = weatherData["attributes"]["daily_forecast"]["feels_like_temp"];
+	  this.humidity = weatherData["attributes"]["daily_forecast"]["humidity"];
+	  this.visibility = weatherData["attributes"]["daily_forecast"]["visibility"];
+	  this.uvIndex = weatherData["attributes"]["daily_forecast"]["uv_index"];
+	  this.longDescription = weatherData["attributes"]["daily_forecast"]["daily_summary"];
+	};
+
+	var UserFavorites = function UserFavorites(favorites) {
+	  _classCallCheck(this, UserFavorites);
+
+	  this.favoriteLocations = favorites;
+	};
+
+	function setUserVariabes() {
+	  getUserApiKey();
+	  getUserFavorites(userApiKey);
+	}
+
+	function getUserApiKey() {
+	  userApiKey = "6808608d5f587d0e83e51e9bbbf4";
+	}
+
+	function displayFavorites() {
+	  var locations = userFavoritesObj.favoriteLocations;
+	  locations.forEach(function (e) {
+	    var name = e["attributes"]["location"];
+	    var newDiv = document.createElement("p");
+	    var newText = document.createTextNode("" + name);
+	    newDiv.appendChild(newText);
+	    document.getElementById("favoritesContainer").appendChild(newDiv);
+	  });
+	}
+
+	function getUserFavorites(api_key) {
+	  var requestUrl = "" + apiUrl + "api/v1/favorites";
+	  var requestResponse = $.ajax({ url: "" + requestUrl,
+	    type: 'get',
+	    contentType: 'application/json',
+	    data: { api_key: "" + userApiKey },
+	    dataType: 'json',
+	    success: function success(res) {
+	      userFavoritesObj = new UserFavorites(res["data"]);
+	      displayFavorites();
+	    },
+	    error: function error(res) {
+	      userFavoritesObj = "Error";
+	    }
+	  });
+	}
+
+	function processWeatherRequest() {
+	  var formattedLocation = weatherInput.value;
+	  var requestUrl = "" + apiUrl + "api/v1/forecast?location=" + formattedLocation;
+	  var requestResponse = $.ajax({ url: "" + requestUrl,
+	    type: 'get',
+	    success: function success(res) {
+	      apiResponse = res["data"];
+	      weatherForecastObj = new WeatherForecast(apiResponse);
+	      displayBoxOne();
+	      displayBoxTwo();
+	    },
+	    error: function error(res) {
+	      apiResponse = "Error";
+	    }
+	  });
+	}
+
+	function displayBoxOne() {
+	  displayDateTime();
+	  document.getElementById("locationName").innerHTML = "" + weatherForecastObj.locationName;
+	  document.getElementById("currentTemp").innerHTML = "Current Temperature: " + weatherForecastObj.currentTemp + " degrees";
+	  document.getElementById("shortWeatherBlurb").innerHTML = "Right Now: " + weatherForecastObj.shortDescription;
+	  document.getElementById("currentTempHigh").innerHTML = "Temperature High: " + weatherForecastObj.currentHigh + " degrees";
+	  document.getElementById("currentTempLow").innerHTML = "Temperature Low: " + weatherForecastObj.currentLow + " degrees";
+	}
+
+	function displayBoxTwo() {
+	  document.getElementById("longWeatherBlurb").innerHTML = "Today: " + weatherForecastObj.longDescription;
+	  document.getElementById("feelsLike").innerHTML = "Feels Like: " + weatherForecastObj.feelsLike + " degrees";
+	  document.getElementById("humidity").innerHTML = "Humidity: " + weatherForecastObj.humidity + "%";
+	  document.getElementById("Visibility").innerHTML = "Visibility: " + weatherForecastObj.visibility + " mi.";
+	  document.getElementById("uvIndex").innerHTML = "UV Index: " + weatherForecastObj.uvIndex;
+	}
+
+	function displayBoxThree() {
+	  displayNextHours();
+	  displayNextDays();
+	}
+
+	function displayNextHours() {}
+
+	function displayDateTime() {
+	  // need to edit this to format unix time returned by DarkSky
+	  document.getElementById("currentTime").innerHTML = "Current Date and Time: " + weatherForecastObj.currentTime;
+	}
+
+	getWeatherButton.addEventListener("click", function () {
+	  processWeatherRequest();
+	});
 
 /***/ })
 /******/ ]);
